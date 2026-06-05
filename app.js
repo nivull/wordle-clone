5fccd20c759a590b4449b0545ff63781d8d8b7a6
nivull <biscaynemac@gmail.com>
2026-06-04 20:25:19 -0400

    functionality of wordle is decent, but need to add visual change

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-let currentColumn = 0;
 let currentRow = 0;
+let currentItem = 0;
+
+let matrix = [  ["", "", "", "", ""],
+                ["", "", "", "", ""],
+                ["", "", "", "", ""],
+                ["", "", "", "", ""],
+                ["", "", "", "", ""] ];
+
+const allowedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+ 
+document.addEventListener('keydown', (event) => {
+    addLetter(event.key.toLowerCase());
+})
+
+function addLetter(key) {
+    if(key == "enter" && matrix[currentRow][4] != "") {   
+        //if submitting line
+        currentItem = 0;
+        submitGuess(currentRow);
+        currentRow++;
+
+    } else if (key == "backspace") {
+        //if backspace
+        matrix[currentRow][currentItem] = "";
+                if(currentItem != 0) {
+            currentItem--;
+        }
+
+    } else if ( allowedCharacters.includes(key) && currentItem <= 4) {
+        //check if allowed character
+        matrix[currentRow][currentItem] = key;
+        if(currentItem < 4) {
+            currentItem++;
+        }     
+    }
+}
+
+function submitGuess() {
+}
