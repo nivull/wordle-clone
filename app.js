714d0d71eb471e7cafa69aeb6596c3771ad159a8
nivull <biscaynemac@gmail.com>
2026-06-04 21:56:26 -0400

    working out duplicates, will probably need to put yellow testing in to a second for loop

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+//the word to guess
+const currentWord = "salad";
+
+//html row elements containing the guessing boxes
+const row1el = document.getElementById("row1");
+const row2el = document.getElementById("row2");
+const row3el = document.getElementById("row3");
+const row4el = document.getElementById("row4");
+const row5el = document.getElementById("row5");
+const row6el = document.getElementById("row6");
+
 let currentRow = 0;
 let currentItem = 0;
+const perRow = 4;
+
+//how many duplicate letters there are, stays constant
+let duplicateLetters = {};
+//variable that will change values while checking a row, resets afterwards
+let duplicateClock = {};
 
 let matrix = [  ["", "", "", "", ""],
                 ["", "", "", "", ""],
@@ -9,15 +26,17 @@ let matrix = [  ["", "", "", "", ""],
 
 const allowedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
  
+setDuplicates();
+
 document.addEventListener('keydown', (event) => {
     addLetter(event.key.toLowerCase());
 })
 
 function addLetter(key) {
-    if(key == "enter" && matrix[currentRow][4] != "") {   
+    if(key == "enter" && matrix[currentRow][perRow] != "") {   
         //if submitting line
         currentItem = 0;
-        submitGuess(currentRow);
+        submitGuess();
         currentRow++;
 
     } else if (key == "backspace") {
@@ -27,14 +46,71 @@ function addLetter(key) {
             currentItem--;
         }
 
-    } else if ( allowedCharacters.includes(key) && currentItem <= 4) {
+    } else if ( allowedCharacters.includes(key) && currentItem <= perRow) {
         //check if allowed character
         matrix[currentRow][currentItem] = key;
-        if(currentItem < 4) {
+        if(currentItem < perRow) {
             currentItem++;
         }     
     }
 }
-
+let test = ['', '', '', '', ''];
 function submitGuess() {
+    // 0 = black, 1 = yellow, 2 = green
+    for(let i = 0; i <= perRow; i++ ) {
+
+        //test green
+        if(matrix[currentRow][i] == currentWord[i]) {
+            test[i] = 2;
+
+        //test yellow
+        } else if (currentWord.includes(matrix[currentRow][i]) && checkDuplicates(matrix[currentRow][i])){
+            test[i] = 1;
+        //black
+        } else {
+            test[i] = 0;
+        }
+    }
+    //reset duplicate clock
+    duplicateClock = duplicateLetters;
+    console.log(test);
+
+}
+
+function checkDuplicates(char) {
+//is it a letter with a duplicate
+    if(duplicateClock[char]) {
+//if duplicate still has another, meaning we can say it's yellow 
+        if(duplicateClock[char] > 1) {
+            duplicateClock[char] -=1;
+            return true;
+//if it is listed as a duplicate but weve already marked, false 
+        } else{
+            return false;
+        }
+    } else{
+        return true;
+    }
+
+
+
+}
+
+function setDuplicates() { 
+
+    for(const letter of currentWord) {
+
+        duplicateLetters[letter] = (duplicateLetters[letter] || 0) + 1;
+ 
+    }
+    //set to key, value pairs ([[p, 1], [z,2]])
+    duplicateLetters = Object.entries(duplicateLetters)
+    //gets rid of non duplicates to add to object array, letter = key to remove, count = number attatched
+    .filter( ([letter, count]) => count > 1)
+    //transforms  key value pair array to object
+    .map( ([letter, count]) => ({letter: letter, count: count}));
+
+    console.log(duplicateLetters);
+
+
 }
